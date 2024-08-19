d95ea2d69ee98ea4ff384bb2de92d2f9c1ca9628
ziauddin-0987 <ziauddink100@gmail.com>
2024-08-19 21:22:01 +0500

    this is new form

--- Electronics/src/App.jsx
+++ Electronics/src/App.jsx
@@ -4,42 +4,124 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  // const inptusRff = useRef(inpurt)
-  function inpus() {
-    const inptusRff = useRef(inpurt);
-    return (inptusRff.style.border = "none");
-  }
   return (
     <>
-      <div className="reactForm">
-        <form>
-          <label htmlFor="karaya">کرایہ دار کا نام </label>
-          <br />
-          <input type="text" placeholder="کرایہ دار کا نام " id="karaya" />
-          <br />
-          <label htmlFor="homeAddras">گھر کا پتہ </label>
-          <br />
-          <input type="text" placeholder="گھر کا پتہ " id="homeAddras" />
-          <br />
-          <label htmlFor="homeAddras">پہلا ضامن </label>
-          <br />
-          <input type="text" placeholder="پہلا ضامن " id="homeAddras" />
-          <br />
-          <label htmlFor="homeAddras">گھر کا پتہ </label>
-          <br />
-          <input type="text" placeholder="گھر کا پتہ " id="homeAddras" />
-          <br />
-          <label htmlFor="homeAddras">دوسرا ضامن </label>
-          <br />
-          <input type="text" placeholder="دوسرا  ضامن " id="homeAddras" />
-          <br />
+      <from>
+        <div className="reactForm">
+          <table>
+            <tr>
+              <th colspan="3">Name of Inspector</th>
+              {/* <th>Guarantor's account no</th>
+              <th>Account no </th> */}
+            </tr>
+            <tr>
+              <td colspan="2">
+                {/* <label htmlFor="Tenant">Tenant Name</label> */}
+                <br />
+                <input type="text" placeholder="Tenant Name " id="Tenant" />
+              </td>
+              <td>
+                {/* <label htmlFor="firstFatherName">Father Name</label> */}
+                <br />
+                <input
+                  type="text"
+                  placeholder="Father Name "
+                  id="firstFatherName"
+                />
+              </td>
+              {/* <td></td> */}
+            </tr>
+            <tr>
+              <td colspan="3">
+                {/* <label htmlFor="firstHomeAddress">Home Address</label> */}
+                <br />
+                <input
+                  type="text"
+                  placeholder="Home Address"
+                  id="firstHomeAddress"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2">
+                {/* <label htmlFor="firstGuarantor">First guarantor</label> */}
+                <br />
+                <input
+                  type="text"
+                  placeholder="First guarantor"
+                  id="frstGuarantor"
+                />
+              </td>
+              <td>
+                {/* <label htmlFor="firstGuaranFather">Father Name</label> */}
+                <br></br>
 
-          <label htmlFor="homeAddras">گھر کا پتہ </label>
-          <br />
-          <input type="text" placeholder="گھر کا پتہ " id="homeAddras" />
-          <br />
-        </form>
-      </div>
+                <input
+                  type="text"
+                  placeholder="Father Name "
+                  id="firstGuaranFather"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2">
+                {/* <label htmlFor="secGuarantor">Second guarantor</label> */}
+                <br />
+                <input
+                  type="text"
+                  placeholder="Second guarantor"
+                  id="secGuarantor"
+                />
+              </td>
+              <td colspan="2">
+                {/* <label
+                  htmlFor="
+                secFatherName"
+                >
+                  Father Name
+                </label> */}
+                <br></br>
+                <input
+                  type="text"
+                  placeholder="Father Name "
+                  id="secFatherName"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td colspan="3">
+                {/* <label htmlFor="SecHomeAddress">Home Address</label> */}
+                <br />
+                <input
+                  type="text"
+                  placeholder="Home Address"
+                  id="secHomeAddress"
+                />
+              </td>
+            </tr>
+            {/* <tr>
+             <td colspan="0  ">
+                <input type="text" className="zia" />
+              </td>
+              <td>
+                <input type="text" className="zia" />
+              </td>
+              <td>
+                <input type="text" className="zia" />
+              </td>
+              <td>
+                <input type="text" className="zia" />
+              </td>
+              <td>
+                <input type="text" className="zia" />
+              </td>
+            </tr> */}
+            <div className="btn">
+              <button type="submit">submit</button>
+            </div>
+          </table>
+        </div>
+      </from>
     </>
   );
 }
---
+++ Electronics/src/nes.jsx
@@ -0,0 +1,47 @@
+ <div className="reactFullForm">
+        
+<div className="formLabel">
+  <ul>
+    <li>Name of Inspector</li>
+    <li>Guarantor's account no</li>
+    <li> Account no </li>
+  </ul>
+</div>
+<div className="reactForm">
+  <form>
+    <label htmlFor="karaya">Tenant Name</label>
+    <br />
+    <input type="text" placeholder="Tenant Name " id="karaya" />
+    <br />
+    <label htmlFor="homeAddress">Home Address</label>
+    <br />
+    <input type="text" placeholder="Home Address" id="homeAddress" />
+    <br />
+    <label htmlFor="homeAddress">First guarantor</label>
+    <br />
+    <input type="text" placeholder="First guarantor" id="homeAddress" />
+    <br />
+    <label htmlFor="homeAddress">Home Address</label>
+    <br />
+    <input type="text" placeholder="Home Address" id="homeAddress" />
+    <br />
+    <label htmlFor="homeAddress">Second guarantor</label>
+    <br />
+    <input
+      type="text"
+      placeholder="Second guarantor"
+      id="homeAddress"
+    />
+    <br />
+
+    <label htmlFor="homeAddress">Second guarantor</label>
+    <br />
+    <input
+      type="text"
+      placeholder="Second guarantor "
+      id="homeAddress"
+    />
+    <br />
+  </form>
+</div>
+</div> 
